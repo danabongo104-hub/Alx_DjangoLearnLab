066db644a27001143d928cc8f50e8d94fbb2100d
danbongo104-hub <danabongo104@gmail.com>
2026-01-21 20:44:48 +0300

    Add views and template

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import list_books, LibraryListView
+
+urlpatterns = [
+    path('', list_books, name='home'), # home page shows all books
+    path('books/', list_books, name='book-list'), # lists all books
+    path('libraries/<int:pk>/', LibraryListView.as_view(), name='library-detail'), # detail view for a library
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.views.generic import DetailView
+from django.shortcuts import render
+from .models import Book, Library
 # Create your views here.
+# function based view to list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/book_list.html', {'books': books})
+
+# class based view to list all libraries
+class LibraryListView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+
+    
