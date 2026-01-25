258ef4634736754f1798e0a0aa49981c841bfc23
danbongo104-hub <danabongo104@gmail.com>
2026-01-25 22:31:43 +0300

    Add authentication

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Author(models.Model):
@@ -24,3 +27,19 @@ class Library(models.Model):
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+
+    def __str__(self):
+        return str(self.name) if self.name else "Unnamed Librarian"
+    
+#userprofile model to extend the default user model
+class UserProfile(models.Model):
+    ROLE = [('member', 'Member'), ('librarian', 'Librarian'),('admin', 'Admin')]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50,choices=ROLE, default='member')
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+#automatic creation of user profile when a user is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
