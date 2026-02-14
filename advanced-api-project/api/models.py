da7f41dbbe4a65898c55253f910d6bd6bc601c1d
danbongo104-hub <danabongo104@gmail.com>
2026-02-14 15:55:06 +0300

    Add advanced api

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} by {self.author.name}"
